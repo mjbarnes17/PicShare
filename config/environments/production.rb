474822c61e7b4652072fdaae0ae9e8daa615f09a
Marcus Barnes <mavjbarnes@gmail.com>
2015-07-18 07:44:14 +0000

    installed the devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # device heroku url for production environment
+  config.action_mailer.default_url_options = { host: 'picshare217.herokuapp.com/' }
 end
